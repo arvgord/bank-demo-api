df1ad7140c6f3fd9c89456f519f56ef778ac54d6
Artem Gordienko <arvgord@gmail.com>
2023-01-07 21:00:43 +0700

    feat: add npm packaging

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.arvgord"
-version = "0.0.2"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -34,12 +34,24 @@ protobuf {
         id("grpckt") {
             artifact = "io.grpc:protoc-gen-grpc-kotlin:${property("grpcKotlinVersion")}:jdk8@jar"
         }
+        id("protoc-gen-js") {
+            path = projectDir.path.plus("/protoc-gen/protoc-gen-js-3.21.2-linux-x86_64")
+        }
+        id("protoc-gen-grpc-web") {
+            path = projectDir.path.plus("/protoc-gen/protoc-gen-grpc-web-1.4.2-linux-x86_64")
+        }
     }
     generateProtoTasks {
         all().forEach {
             it.plugins {
                 id("grpc")
                 id("grpckt")
+                id("protoc-gen-js") {
+                    option("import_style=commonjs,binary")
+                }
+                id("protoc-gen-grpc-web") {
+                    option("import_style=commonjs+dts,mode=grpcweb")
+                }
             }
             it.builtins {
                 id("kotlin")
@@ -47,9 +59,6 @@ protobuf {
         }
     }
 }
-
-tasks.named("processResources").configure { dependsOn("generateProto") }
-tasks.named("extractTestProto").configure { dependsOn("generateProto") }
 
 sourceSets {
     main {
@@ -80,3 +89,20 @@ publishing {
         }
     }
 }
+
+tasks.register<Copy>("buildAndCopy") {
+    from(
+        projectDir.path.plus("/build/generated/source/proto/main/protoc-gen-js"),
+        projectDir.path.plus("/build/generated/source/proto/main/protoc-gen-grpc-web")
+    )
+    into(projectDir.path.plus("/npm-package/"))
+}
+tasks.register<Delete>("cleanNpmDir") {
+    delete(projectDir.path.plus("/npm-package/bankdemo/"))
+    delete(projectDir.path.plus("/npm-package/node_modules/"))
+    delete(projectDir.path.plus("/npm-package/package-lock.json"))
+}
+tasks["buildAndCopy"].dependsOn("build")
+tasks["clean"].dependsOn("cleanNpmDir")
+tasks["processResources"].dependsOn("generateProto")
+tasks["extractTestProto"].dependsOn("generateProto")
